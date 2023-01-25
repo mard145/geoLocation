26fa60e43439145faf72f950d5fc1c4ed33984c4
mard145 <slave1989@hotmail.com>
2023-01-25 18:01:33 -0300

    att

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const path = require('path')
 const PORT = 3333
 const fs = require('fs');
 const axios = require('axios');
-
+const apiip = require('apiip.net')('556efcd7-b691-4f58-9a6e-d02045e73dc8');
+apiip
+  .getLocation()
+  .then((results) => console.log(results))
+  .catch((error) => console.error(error));
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-axios.get('https://ipgeolocation.abstractapi.com/v1/?api_key=92486756521446fba895daa9f1d7e8a9')
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+// axios.get('https://ipgeolocation.abstractapi.com/v1/?api_key=92486756521446fba895daa9f1d7e8a9')
+//     .then(response => {
+//         console.log(response.data);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
 
 app.get('/', (req, res)=>{
     res.render('index')
